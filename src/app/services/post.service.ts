e2ac5bd361d3727352341ec62b063baae1269fdd
Lilsant <lilsant@mail.ru>
2023-03-31 11:28:02 +0300

    few improves and fixes

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -5,15 +5,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class PostService {
+  apiKey: string = 'a72cfd6a8596471da14dc61002297a2c';
   constructor(private http: HttpClient) {}
   getPosts(pageNumber: number, searchRequest: string) {
     return this.http.get(
-      `https://newsapi.org/v2/everything?q=${searchRequest}&pageSize=4&page=${pageNumber}&apiKey=6b5d306006d54ba797dbdf1643b40930`
+      `https://newsapi.org/v2/everything?q=${searchRequest}&pageSize=4&page=${pageNumber}&apiKey=${this.apiKey}`
     );
   }
   getTopPosts(category: string, country: string, pageNumber: number) {
     return this.http.get(
-      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=4&page=${pageNumber}&apiKey=6b5d306006d54ba797dbdf1643b40930`
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=4&page=${pageNumber}&apiKey=${this.apiKey}`
     );
   }
 }
